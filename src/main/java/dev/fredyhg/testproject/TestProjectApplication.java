79f48b9f450446dc3445ac0a0c9f7a504724e1ee
unknown <fredyultima2@gmail.com>
2024-02-10 04:02:39 -0300

    second commit

--- src/main/java/dev/fredyhg/testproject/TestProjectApplication.java
+++ src/main/java/dev/fredyhg/testproject/TestProjectApplication.java
@@ -1,13 +1,26 @@
 package dev.fredyhg.testproject;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Date;
+
+
+@EnableScheduling
 @SpringBootApplication
+@Slf4j
 public class TestProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestProjectApplication.class, args);
+    }
+
+    @Scheduled(cron = "0 20 4 * * ?")
+    public void testSchedule(){
+        log.info(String.valueOf(new Date()));
     }
 
 }
